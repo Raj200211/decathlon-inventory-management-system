9c38e9e4b5c703924e1c04a248bced66ec9a8a37
Raj200211 <rajkd2811@gmail.com>
2023-11-16 20:57:14 +0530

    first commit

---
+++ dashboard/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Product, Order
+from django.contrib.auth.models import Group
+
+admin.site.site_header = 'DECATHLON DATABASE'
+
+class productadmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'category')
+
+    list_filter = ['category']
+
+# Register your models here.
+
+admin.site.register(Product, productadmin)
+
+admin.site.register(Order)
+
+admin.site.unregister(Group)
